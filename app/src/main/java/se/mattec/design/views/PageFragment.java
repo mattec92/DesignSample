6267ca35cbffccfa6922b50d245e4a0680566af2
Mattias Cederlund <mattec92@hotmail.com>
2016-12-03 14:51:15 +0100

    Changed card content to a scrollview and added margins between pages.

--- app/src/main/java/se/mattec/design/views/PageFragment.java
+++ app/src/main/java/se/mattec/design/views/PageFragment.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,8 +20,8 @@ public class PageFragment
     @BindView(R.id.card)
     protected CardView mCardView;
 
-    @BindView(R.id.card_content)
-    protected LinearLayout mCardContent;
+    @BindView(R.id.scrollview)
+    protected ScrollView mScrollView;
 
     public static PageFragment newInstance()
     {
